fc347f9a2563c594f40ac49e721e1775aeb6c495
FeliBlanco <felipeblanco09@hotmail.com>
2023-09-12 23:06:40 -0300

    1.0.3

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 import { useState } from "react";
-import { TouchableHighlight, StyleSheet } from "react-native";
+import { TouchableHighlight, StyleSheet, View } from "react-native";
 
-export default function CircleCheckBox() {
+export default function CircleCheckBox({size = 20, onChangeValue, activateColor = "red", color = "red"}) {
+
+    const [activate, setActivate] = useState(false)
+
+    let circleSize = (size * 50) / 100;
+
+
+    const _click = () => {
+        if(onChangeValue) {
+            setActivate(i => !i)
+            onChangeValue(activate)
+        }
+    }
+
     return (
-        <TouchableHighlight>
-
+        <TouchableHighlight onPress={() => _click() } style={{width: size, height: size, borderRadius: size, borderColor: activate ? activateColor : color, borderWidth:3, justifyContent:'center', alignItems:'center'}}>
+            {
+                activate ?
+                    <View style={{width: circleSize, height: circleSize, borderRadius: circleSize, backgroundColor: activateColor}}></View>
+                :
+                    <></>
+            }
         </TouchableHighlight>
     )
 }
