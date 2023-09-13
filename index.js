0ca9da066b9b058d1a098ccf1a8dea05cd4d3146
FeliBlanco <felipeblanco09@hotmail.com>
2023-09-12 22:07:41 -0300

    hello word

---
+++ index.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { TouchableHighlight } from "react-native";
+
+export default function CircleCheckBox() {
+    return (
+        <TouchableHighlight>
+
+        </TouchableHighlight>
+    )
+}
